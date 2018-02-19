cab8cdebbbcbf70999690487d3fbbae049a65aa9
Daniel Roberts <BeardOfDan@gmail.com>
2018-02-18 23:13:33 -0800

    (feat) Do more work on Google Oauth

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const app = express();
 
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const KEYS = require('./config/keys');
 
 const PORT = process.env.PORT || 3000;
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      'clientID': KEYS.googleClientID,
+      'clientSecret': KEYS.googleClientSecret,
+      'callbackURL': '/auth/google/callback'
+    }, (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
-
-
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    'scope': ['profile', 'email']
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
