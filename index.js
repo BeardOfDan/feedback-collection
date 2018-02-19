258de5f389f244ddaabe1fd8ab05128dd293e9fc
Daniel Roberts <BeardOfDan@gmail.com>
2018-02-18 23:43:47 -0800

    (feat) Add script start-dev that uses nodemon

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 
@@ -14,18 +13,24 @@ passport.use(
       'clientID': KEYS.googleClientID,
       'clientSecret': KEYS.googleClientSecret,
       'callbackURL': '/auth/google/callback'
-    }, (accessToken) => {
-      console.log(accessToken);
+    }, (accessToken, refreshToken, profile, done) => {
+      console.log('access token:', accessToken, '\n\n');
+      console.log('refresh token:', refreshToken, '\n\n');
+      console.log('profile:', profile, '\n\n');
     }
   )
 );
 
+// Get Authentication Code from Google
 app.get(
   '/auth/google',
   passport.authenticate('google', {
     'scope': ['profile', 'email']
   })
 );
+
+// Utilize Google's Authenication Code
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
