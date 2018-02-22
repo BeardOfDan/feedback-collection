eb2376c338e419250e97178232bb7b12bdfef42d
Daniel Roberts <BeardOfDan@gmail.com>
2018-02-22 03:02:31 -0800

    (feat) Create initial React content
    This is in a directory called 'client'
    This was done with create-react-app

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookieSession = require('cookie-session');
 // set up local environment variables
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const KEYS = require('./config/keys');
 
 app.use(
