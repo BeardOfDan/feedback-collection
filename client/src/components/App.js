be14173a42b8b5be7a57d6ddfabff38d8ec71242
Daniel Roberts <BeardOfDan@gmail.com>
2018-02-25 14:43:28 -0800

    (refactor) Seperate the Header component into its own file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <h2>Header</h2>
-  );
-};
+import Header from './Header';
 
 const Dashboard = () => {
   return (
