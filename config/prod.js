54226120734e6d10255d024a5dea138a073c3288
Daniel Roberts <BeardOfDan@gmail.com>
2018-03-04 18:29:17 -0800

    (feat) Add stripe key fields to config/prod.js

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
   'googleClientID': process.env.GOOGLE_CLIENT_ID,
   'googleClientSecret': process.env.GOOGLE_CLIENT_SECRET,
   'mongoURI': `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`,
-  'cookieKey': process.env.COOKIE_KEY
+  'cookieKey': process.env.COOKIE_KEY,
+  'stripePublishableKey': process.env.STRIPE_PUBLISHABLE_KEY,
+  'stripeSecretKey': process.env.STRIPE_SECRET_KEY
 };
