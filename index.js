0ab98797f07f5a9f9a566b4e25c474499fe4361e
Daniel Roberts <BeardOfDan@gmail.com>
2018-03-11 19:28:19 -0700

    (feat) Ensures that production code uses the 'build' of client

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ require('./routes/authRoutes')(app);
 // setup Stripe routes
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Ensures that express knows to use the 'build' of the client side code
+  app.use(express.static('client/build'));
+
+  // Ensures that React Router handles any unhandled paths
+  const path = require('path');
+  app.get('*', (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT} \n`);
 });
