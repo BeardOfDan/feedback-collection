fa8c304f093d6150d67fff8b26e2131db41e0730
Daniel Roberts <BeardOfDan@gmail.com>
2018-03-20 17:08:46 -0700

    (feat) Form most of the logic for the route '/api/surveys'

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,25 @@
+const mongoose = require('mongoose');
+const Survey = mongoose.model('surveys');
+
 const requireLogin = require('./../middlewares/requireLogin');
 const requireCredits = require('./../middlewares/requireCredits');
 
 module.exports = (app) => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res, next) => {
+    const { title, subject, body, recipients } = req.body;
 
+    // translates a csv string into an array of objects
+    const recipientsArray = recipients.split(',').map((email) => {
+      return { 'email': email.trim() };
+    });
+
+    const survey = new Survey({
+      title,
+      body,
+      subject,
+      'recipients': recipientsArray,
+      '_user': req.user.id,
+      'dateSent': Date.now()
+    });
   });
 };
